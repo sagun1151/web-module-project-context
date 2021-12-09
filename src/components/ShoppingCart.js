39c868306e3563b592d8f76413e0bd017d3e52e1
sagun-shrestha <sshrestha426699@gmail.com>
2021-12-09 14:58:32 -0700

    all done

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../contexts/CartContext';
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const { cart, setCart } = useContext(CartContext);
+
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
+	const remove =(id)=>{
+		console.log('id',id);
+		const newCart = cart.filter((item) => item.id !== id);
+		console.log('newCart', newCart)
+		setCart(newCart);
+	}
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
+			{cart.map(item => (
+				<Item key={item.id} {...item} remove={remove}/>
 			))}
 
 			<div className="shopping-cart__checkout">
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Item = props => {
+	console.log('props',props);
+
+	const handleClick = (e)=> {
+		console.log('e', e.target.id);
+		props.remove(e.target.id)
+	}
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +16,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={handleClick} id={props.id}>Remove from cart</button>
 			</div>
 		</div>
 	);
